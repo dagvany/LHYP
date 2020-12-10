450d41526ed377c11a79eed267c6737026bf4c4f
Demeter Peter <demeter.333@gmail.com>
2020-12-10 13:41:00 +0100

    repair laconvae activation fnc setting

--- src/laconvae.py
+++ src/laconvae.py
@@ -6,52 +6,54 @@ from torch import nn
 
 class LaConvAE(nn.Module):
     def getNameOfActivation():
-        return "SELU"
+        return self.activation()._get_name()
     
     def __init__(self):
         super(LaConvAE, self).__init__()
+        
+        self.activation = nn.SELU
 
         self.encoderConv = nn.Sequential(
             nn.BatchNorm2d(1),
             nn.Conv2d(1, 8, kernel_size=2, stride=1, padding=0),
-            nn.getattr(self.getNameOfActivation(), nn)(),
+            self.activation(),
 
             nn.BatchNorm2d(8),
             nn.Conv2d(8, 16, kernel_size=2, stride=1, padding=0),
-            nn.getattr(self.getNameOfActivation(), nn)(),
+            self.activation(),
 
             nn.BatchNorm2d(16),
             nn.Conv2d(16, 32, kernel_size=2, stride=1, padding=0),
-            nn.getattr(self.getNameOfActivation(), nn)(),
+            self.activation(),
 
             nn.BatchNorm2d(32),
             nn.Conv2d(32, 16, kernel_size=3, stride=2, padding=0),
-            nn.getattr(self.getNameOfActivation(), nn)(),
+            self.activation(),
 
             nn.BatchNorm2d(16),
             nn.Conv2d(16, 8, kernel_size=3, stride=2, padding=0),
-            nn.getattr(self.getNameOfActivation(), nn)(),
+            self.activation(),
 
             # Instead of MaxPool
             nn.BatchNorm2d(8),
             nn.Conv2d(8, 1, kernel_size=1, stride=1, padding=0),
-            nn.getattr(self.getNameOfActivation(), nn)(),
+            self.activation(),
         )
 
         self.decoderDeConv = nn.Sequential(
             nn.ConvTranspose2d(1, 8, kernel_size=1, stride=1, padding=0),
-            nn.getattr(self.getNameOfActivation(), nn)(),
+            self.activation(),
 
             nn.ConvTranspose2d(8, 16, kernel_size=3, stride=2, padding=0),
-            nn.getattr(self.getNameOfActivation(), nn)(),
+            self.activation(),
             nn.ConvTranspose2d(16, 32, kernel_size=3, stride=2, padding=0),
-            nn.getattr(self.getNameOfActivation(), nn)(),
+            self.activation(),
             nn.ConvTranspose2d(32, 16, kernel_size=2, stride=1, padding=0),
-            nn.getattr(self.getNameOfActivation(), nn)(),
+            self.activation(),
             nn.ConvTranspose2d(16, 8, kernel_size=2, stride=1, padding=0),
-            nn.getattr(self.getNameOfActivation(), nn)(),
+            self.activation(),
             nn.ConvTranspose2d(8, 1, kernel_size=2, stride=1, padding=0),
-            nn.getattr(self.getNameOfActivation(), nn)(),
+            self.activation(),
         )
 
         for m in self.modules():
